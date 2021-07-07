fea3b77ab326345fc4f9553bea847b3c56c71d74
Conrad Bockhop <bockhopc@gmail.com>
2021-07-07 06:56:11 -0700

    first commit

---
+++ weatherapp.py
@@ -0,0 +1,66 @@
+from tkinter import *
+from tkinter import messagebox
+from configparser import ConfigParser
+import requests
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid={}'
+
+config_file = 'config.ini'
+config = ConfigParser()
+config.read(config_file)
+api_key = config['api_key']['key']
+
+print(api_key)
+
+def get_weather(city):
+    result = requests.get(url.format(city, api_key))
+    if result:
+        json = result.json()
+        city = json['name']
+        country = json['sys']['country']
+        temp = json['main']['temp']
+        icon = json['weather'][0]['icon']
+        weather = json['weather'][0]
+        ['main']
+        final = (city, country, temp, icon, weather)
+        return final
+
+    else:
+        return None
+
+
+def search():
+    city = city_text.get()
+    weather = get_weather(city)
+    if weather:
+        location_lbl['text'] = f'{weather[0]}, {weather[1]}'
+        image['bitmap'] = f'imgs/{weather[3]}.png'
+        temp_lbl['text'] = f'{weather[2]:.2f}°'
+    else:
+        messagebox.showerror('Error', f'Cannot find city {city}')
+
+# Setting up basic GUI
+app = Tk()
+app.title('Weather App')
+app.geometry('700x350')
+
+city_text = StringVar()
+city_entry = Entry(app, textvariable=city_text)
+city_entry.pack()
+
+search_btn = Button(app, text='Search Weather', width=12, command=search)
+search_btn.pack()
+
+location_lbl = Label(app, text='', font=('bold', 20))
+location_lbl.pack()
+
+image = Label(app, bitmap='')
+image.pack()
+
+temp_lbl = Label(app, text='')
+temp_lbl.pack()
+
+weather_lbl = Label(app, text='')
+weather_lbl.pack()
+
+app.mainloop()
